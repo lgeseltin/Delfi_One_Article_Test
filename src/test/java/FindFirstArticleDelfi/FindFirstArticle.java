67326ca179c16f1b2fd25c80a6fe3333f95cda91
lgeseltin <ee@ee.ee>
2018-04-24 00:23:13 +0300

    Clean up

--- src/test/java/FindFirstArticleDelfi/FindFirstArticle.java
+++ src/test/java/FindFirstArticleDelfi/FindFirstArticle.java
@@ -2,9 +2,9 @@ package FindFirstArticleDelfi;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import pages.BaseFunc;
 import pages.HomePage;
@@ -14,17 +14,23 @@ import java.util.List;
 public class FindFirstArticle {
     private BaseFunc baseFunc = new BaseFunc();
     private static final Logger LOGGER = LogManager.getLogger(FindFirstArticle.class);
+    public static final String URL = "www.delfi.lv";
+    public static final String ARTICLE_TO_FIND = "Sevastova un Ostapenko ieved Latvijas komandu Federāciju kausa pasaules grupā";
 
     @Test
     public void checkArticlePresent() {
-        baseFunc.goToUrl(HomePage.URL);
+        baseFunc.goToUrl(URL);
 
-        List<WebElement> titles = baseFunc.getElements(HomePage.TITLE);
+        //Konstruktor
+        HomePage homePage = new HomePage(baseFunc);
+
+        List<WebElement> titles = homePage.getArticles();
+
         boolean isArticleFound = false;
 
         for (WebElement we : titles) {
             LOGGER.info("Founded Article " + titles.indexOf(we) + ": " + we.getText());
-            if (we.getText().equals(HomePage.ARTICLE_TO_FIND)) {
+            if (we.getText().equals(ARTICLE_TO_FIND)) {
                 isArticleFound = true;
                 break;
             }
@@ -32,6 +38,11 @@ public class FindFirstArticle {
 
         Assert.assertTrue("No article found", isArticleFound);
         LOGGER.info("We found our Article");
+    }
+
+    @After
+    public void closeBrowser() {
         baseFunc.quitDriver();
     }
+
 }
--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -1,9 +1,19 @@
 package pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 public class HomePage {
-    public static final String ARTICLE_TO_FIND = "Latvijas hokeja izlase zaudē Slovākijai arī otrajā pārbaudes spēlē";
+    BaseFunc baseFunc;
     public static final By TITLE = By.xpath("//a[contains(@class, 'top2012-title')]");
-    public static final String URL = "www.delfi.lv";
+
+    public HomePage(BaseFunc baseFunc) {
+        this.baseFunc = baseFunc;
+    }
+
+    public List<WebElement> getArticles() {
+        return baseFunc.getElements(TITLE);
+    }
 }
